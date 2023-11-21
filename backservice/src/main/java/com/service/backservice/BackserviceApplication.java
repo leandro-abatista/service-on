5bd4803e079e0e3da371abc2be4ece33fe92d9b9
Leandro Amorim <leandroamorimpb@gmail.com>
2023-11-21 16:46:55 -0300

    Primeiro commit do projeto Sistema de gestão de serviços 🔒

---
+++ backservice/src/main/java/com/service/backservice/BackserviceApplication.java
@@ -0,0 +1,13 @@
+package com.service.backservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackserviceApplication.class, args);
+	}
+
+}
